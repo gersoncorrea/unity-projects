e1d64d28c6999deeca1a22c51e65fb08a03974c7
gersoncorrea <gersoncorrea.jr@gmail.com>
2018-05-06 20:20:50 -0400

    preparando cena e movendo bola

---
+++ BolaRolante/Assets/MovimentaBola.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentaBola : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float velocidade;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 move = new Vector3 (Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+		rb.AddForce (move*velocidade);
+	}
+}
